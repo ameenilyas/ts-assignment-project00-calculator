abe078ca6f0cab01d03df70f6304178acd502274
Ameen Ilyas <ameenilyas786q.sols@gmail.com>
2022-10-12 19:22:49 +0500

    did some work on students and added some options on it

--- project06_student_management_system.js
+++ project06_student_management_system.js
@@ -1,16 +1,33 @@
 import inquirer from "inquirer";
+// enum StatusEnum {
+//   "Student Options" = "student_options",
+//   "View Students" = "view_students",
+//   "Add Student" = "add_student",
+//   "Exit" = "exit",
+// }
+const main_options = ["view_students", "add_student", "exit"];
+let students_options = [];
+let student_options = [
+    "enroll",
+    "view balance",
+    "pay tution fee",
+    "show status",
+    "exit",
+];
 const students = [];
-let status;
+let status = "main_options";
 class Student {
     id;
     name;
     courses_enroll;
     balance;
+    is_fee_paid;
     constructor(name) {
         this.id = this.generateUUID() || "00001";
         this.name = name;
         this.courses_enroll = [];
         this.balance = 0;
+        this.is_fee_paid = false;
     }
     generateUUID() {
         const id = Math.floor(Math.random() * 99999) + "";
@@ -26,17 +43,17 @@ class Student {
     balance: ${this.balance}`);
     }
 }
-async function studentOption() {
+async function studentOption(options) {
     const answers = await inquirer.prompt({
         name: "student_option",
         type: "list",
-        message: `Select your Currency to convert.`,
-        choices: ["View Student", "Add Student", "Exit"],
+        message: `Select option.`,
+        choices: options,
         default() {
-            return "View Student";
+            return "view students";
         },
     });
-    return answers.currencies_options;
+    return answers.student_option;
 }
 async function addStudent() {
     const answers = await inquirer.prompt({
@@ -48,4 +65,33 @@ async function addStudent() {
         },
     });
     return answers.student;
+}
+while (status !== "exit") {
+    const _name = students_options.find((name) => status === name) || "";
+    switch (status) {
+        case "main_options":
+            status = await studentOption(main_options);
+            break;
+        case "view_students":
+            students_options = students
+                .map((students) => students.name)
+                .concat(["main_options", "exit"]);
+            status = await studentOption(students_options);
+            break;
+        case "add_student":
+            const student_name = (await addStudent()).trim();
+            const student = new Student(student_name);
+            students.push(student);
+            console.log(`Student ${student_name} added successfully.`);
+            status = "main_options";
+            break;
+        case _name: // dynamic Check
+            // const _student = students.find(student => student.name === _name);
+            status = await studentOption(student_options);
+            break;
+        case "exit":
+            process.exit(0);
+        default:
+            break;
+    }
 }
--- project06_student_management_system.ts
+++ project06_student_management_system.ts
@@ -3,25 +3,45 @@ import inquirer from "inquirer";
 interface StudentInterface {
   id: string;
   name: string;
-  courses_enroll: string[] | [];
+  is_fee_paid: boolean;
+  courses_enroll: string[];
   balance: number;
   showStatus: Function;
 }
 
+// enum StatusEnum {
+//   "Student Options" = "student_options",
+//   "View Students" = "view_students",
+//   "Add Student" = "add_student",
+//   "Exit" = "exit",
+// }
+
+const main_options: string[] = ["view_students", "add_student", "exit"];
+let students_options: string[] = [];
+let student_options: string[] = [
+  "enroll",
+  "view balance",
+  "pay tution fee",
+  "show status",
+  "exit",
+];
+
 const students: StudentInterface[] = [];
-let status: string;
+let status: string = "main_options";
 
 class Student implements StudentInterface {
   public id: string;
   public name: string;
-  public courses_enroll: string[] | [];
+  public courses_enroll: string[];
   public balance: number;
+  public is_fee_paid: boolean;
 
   constructor(name: string) {
     this.id = this.generateUUID() || "00001";
     this.name = name;
     this.courses_enroll = [];
     this.balance = 0;
+    this.is_fee_paid = false;
   }
 
   protected generateUUID() {
@@ -39,17 +59,17 @@ class Student implements StudentInterface {
   }
 }
 
-async function studentOption(): Promise<string> {
+async function studentOption(options: string[]): Promise<string> {
   const answers = await inquirer.prompt({
     name: "student_option",
     type: "list",
-    message: `Select your Currency to convert.`,
-    choices: ["View Student", "Add Student", "Exit"],
+    message: `Select option.`,
+    choices: options,
     default() {
-      return "View Student";
+      return "view students";
     },
   });
-  return answers.currencies_options;
+  return answers.student_option;
 }
 
 async function addStudent(): Promise<string> {
@@ -62,4 +82,38 @@ async function addStudent(): Promise<string> {
     },
   });
   return answers.student;
+}
+while (status !== "exit") {
+  const _name: string = students_options.find((name) => status === name) || "";
+  switch (status) {
+    case "main_options":
+      status = await studentOption(main_options);
+
+      break;
+    case "view_students":
+      students_options = students
+        .map((students) => students.name)
+        .concat(["main_options", "exit"]);
+      status = await studentOption(students_options);
+
+      break;
+    case "add_student":
+      const student_name = (await addStudent()).trim();
+      const student = new Student(student_name);
+      students.push(student);
+
+      console.log(`Student ${student_name} added successfully.`);
+      status = "main_options";
+      break;
+    case _name: // dynamic Check
+      // const _student = students.find(student => student.name === _name);
+      status = await studentOption(student_options);
+      break;
+
+    case "exit":
+      process.exit(0);
+
+    default:
+      break;
+  }
 }
