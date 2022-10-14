3eca3c2e0724abe57a8d3d8ee79cac30d1b18f72
Ameen Ilyas <ameenilyas786q.sols@gmail.com>
2022-10-14 19:12:41 +0500

    updated

--- project06_student_management_system.js
+++ project06_student_management_system.js
@@ -1,9 +1,4 @@
 import inquirer from "inquirer";
-const main_menu = [
-    "view_students",
-    "add_student",
-    "~exit",
-];
 const courses = [
     {
         course_id: Math.floor(Math.random() * 10000).toString(),
@@ -24,12 +19,22 @@ const courses = [
         is_fee_paid: false,
     },
 ];
+const main_menu = [
+    "view_students",
+    "add_student",
+    "~exit",
+];
 const student_options = [
     "show status",
     "my courses",
     "enroll",
-    "view balance",
-    "pay tution fee",
+    // "view balance",
+    "~main_menu",
+    "~exit",
+];
+let course_options = [
+    "course status",
+    "pay fee",
     "~main_menu",
     "~exit",
 ];
@@ -40,14 +45,12 @@ class Student {
     id;
     name;
     courses_enroll;
-    balance;
-    is_fee_paid;
+    // public balance: number;
     constructor(name) {
         this.id = this.generateUUID() || "00001";
         this.name = name;
         this.courses_enroll = [];
-        this.balance = 0;
-        this.is_fee_paid = false;
+        // this.balance = 0;
     }
     generateUUID() {
         const id = Math.floor(Math.random() * 99999) + "";
@@ -59,12 +62,15 @@ class Student {
         console.log(`
     id: ${this.id}
     name: ${this.name}
-    balance: ${this.balance});
-    is fee paid: ${this.is_fee_paid}`);
+    `);
+        // balance: ${this.balance}
     }
     enroll(course) {
+        const is_enrolled = this.courses_enroll.find((course_enroll) => course_enroll.course_id === course.course_id);
+        if (is_enrolled)
+            return console.log("Enrolled Already.");
         this.courses_enroll.push(course);
-        console.log(`Student enrolled in ${course?.course_name} course Successfully.`);
+        console.log(`Enrolled in ${course?.course_name} course Successfully.`);
     }
 }
 async function studentOption(options) {
@@ -92,8 +98,10 @@ async function addStudent() {
 }
 let _student;
 let _course;
+let previous_option = "";
 while (status !== "~exit") {
     const _student_name = students_options.find((name) => name === status) || "";
+    console.log({ status, students, _student });
     switch (status) {
         case "~main_menu":
             status = await studentOption(main_menu);
@@ -123,18 +131,25 @@ while (status !== "~exit") {
                 .concat(["~main_menu", "~exit"]);
             status = await studentOption(courses_name);
             break;
-        case "view balance":
-            console.log(_student?.balance);
-            status = await studentOption(student_options);
-            break;
-        case "pay tution fee":
-            console.log(_student?.id);
-            status = await studentOption(student_options);
-            break;
+        // case "view balance":
+        //   console.log(_student?.balance);
+        //   status = await studentOption(student_options);
+        //   break;
         case "my courses":
-            const my_courses = _student?.courses_enroll.map((course) => course.course_name);
-            console.log(`You have enrolled in these courses, ${my_courses}`);
-            status = await studentOption(student_options);
+            const my_courses = _student?.courses_enroll
+                .map((course) => course.course_name)
+                .concat(["~main_menu", "~exit"]);
+            if (my_courses?.length && my_courses?.length > 2) {
+                console.log(`You have enrolled in these courses, ${my_courses}`);
+                const course_name = await studentOption(my_courses);
+                const updated_course = courses.find((course) => course.course_name === course_name);
+                _course = updated_course ? { ...updated_course } : undefined;
+                status = `${course_name}-enrolled`;
+            }
+            else {
+                console.log(`You are not currently enrolled in any course, select enroll option to participate.`);
+                status = await studentOption(student_options);
+            }
             break;
         case "show status":
             _student?.showStatus();
@@ -142,19 +157,61 @@ while (status !== "~exit") {
             break;
         // enrolling in the course.
         case "metaverse":
-            _course = courses.find((course) => course.course_name === "metaverse");
-            _student?.enroll(_course);
-            status = await studentOption(student_options);
+            {
+                const updated_course = courses.find((course) => course.course_name === "metaverse");
+                _course = updated_course ? { ...updated_course } : undefined;
+                _student?.enroll(_course);
+                status = await studentOption(student_options);
+            }
             break;
         case "blockchain":
-            _course = courses.find((course) => course.course_name === "blockchain");
-            _student?.enroll(_course);
-            status = await studentOption(student_options);
+            {
+                const updated_course = courses.find((course) => course.course_name === "blockchain");
+                _course = updated_course ? { ...updated_course } : undefined;
+                _student?.enroll(_course);
+                status = await studentOption(student_options);
+            }
             break;
         case "cnc":
-            _course = courses.find((course) => course.course_name === "cnc");
-            _student?.enroll(_course);
-            status = await studentOption(student_options);
+            {
+                const updated_course = courses.find((course) => course.course_name === "cnc");
+                _course = updated_course ? { ...updated_course } : undefined;
+                _student?.enroll(_course);
+                status = await studentOption(student_options);
+            }
+            break;
+        // course actions.
+        case "metaverse-enrolled":
+            status = await studentOption(course_options);
+            break;
+        case "blockchain-enrolled":
+            status = await studentOption(course_options);
+            break;
+        case "cnc-enrolled":
+            status = await studentOption(course_options);
+            break;
+        case "course status":
+            console.log({ _course });
+            status = await studentOption(course_options);
+            break;
+        case "pay fee":
+            if (_course?.is_fee_paid) {
+                console.log("Bohut Paisa hen kiya, Already Paid...");
+            }
+            else if (_course && !_course?.is_fee_paid) {
+                _course.is_fee_paid = true;
+                console.log(`Paid Fee Successfully of ${_course?.course_name} course.`);
+                if (_student?.courses_enroll.length) {
+                    const updated_courses_enroll = _student?.courses_enroll.map((course) => course.course_id === _course?.course_id ? _course : course);
+                    const updated_student = {
+                        ..._student,
+                        courses_enroll: updated_courses_enroll,
+                    };
+                    const student_index = students.findIndex((student) => student.id === updated_student.id ? updated_student : student);
+                    students.splice(student_index, 1, updated_student);
+                }
+            }
+            status = await studentOption(course_options);
             break;
         case "~exit":
             process.exit(0);
--- project06_student_management_system.ts
+++ project06_student_management_system.ts
@@ -11,16 +11,11 @@ interface StudentInterface {
   id: string;
   name: string;
   courses_enroll: CoursesInterface[];
-  balance: number;
+  // balance: number;
   showStatus: Function;
   enroll: Function;
 }
 
-const main_menu: [string, string, string] = [
-  "view_students",
-  "add_student",
-  "~exit",
-];
 const courses: [CoursesInterface, CoursesInterface, CoursesInterface] = [
   {
     course_id: Math.floor(Math.random() * 10000).toString(),
@@ -41,17 +36,27 @@ const courses: [CoursesInterface, CoursesInterface, CoursesInterface] = [
     is_fee_paid: false,
   },
 ];
-const student_options: string[] = [
+const main_menu: [string, string, string] = [
+  "view_students",
+  "add_student",
+  "~exit",
+];
+const student_options: [string, string, string, string, string] = [
   "show status",
   "my courses",
   "enroll",
-  "view balance",
-  "pay tution fee",
+  // "view balance",
+  "~main_menu",
+  "~exit",
+];
+
+let course_options: [string, string, string, string] = [
+  "course status",
+  "pay fee",
   "~main_menu",
   "~exit",
 ];
 let students_options: string[] = [];
-
 const students: StudentInterface[] = [];
 let status: string = "~main_menu";
 
@@ -59,15 +64,13 @@ class Student implements StudentInterface {
   public id: string;
   public name: string;
   public courses_enroll: CoursesInterface[];
-  public balance: number;
-  public is_fee_paid: boolean;
+  // public balance: number;
 
   constructor(name: string) {
     this.id = this.generateUUID() || "00001";
     this.name = name;
     this.courses_enroll = [];
-    this.balance = 0;
-    this.is_fee_paid = false;
+    // this.balance = 0;
   }
 
   protected generateUUID() {
@@ -80,16 +83,18 @@ class Student implements StudentInterface {
     console.log(`
     id: ${this.id}
     name: ${this.name}
-    balance: ${this.balance});
-    is fee paid: ${this.is_fee_paid}`);
+    `);
+    // balance: ${this.balance}
   }
 
   enroll(course: CoursesInterface) {
-    this.courses_enroll.push(course);
-
-    console.log(
-      `Student enrolled in ${course?.course_name} course Successfully.`
+    const is_enrolled = this.courses_enroll.find(
+      (course_enroll) => course_enroll.course_id === course.course_id
     );
+    if (is_enrolled) return console.log("Enrolled Already.");
+
+    this.courses_enroll.push(course);
+    console.log(`Enrolled in ${course?.course_name} course Successfully.`);
   }
 }
 
@@ -120,10 +125,13 @@ async function addStudent(): Promise<string> {
 
 let _student: StudentInterface | undefined;
 let _course: CoursesInterface | undefined;
+let previous_option: string = "";
 
 while (status !== "~exit") {
   const _student_name: string =
     students_options.find((name: string) => name === status) || "";
+
+  console.log({ status, students, _student });
 
   switch (status) {
     case "~main_menu":
@@ -161,22 +169,31 @@ while (status !== "~exit") {
 
       status = await studentOption(courses_name);
       break;
-    case "view balance":
-      console.log(_student?.balance);
-      status = await studentOption(student_options);
-      break;
-
-    case "pay tution fee":
-      console.log(_student?.id);
-      status = await studentOption(student_options);
-      break;
+    // case "view balance":
+    //   console.log(_student?.balance);
+    //   status = await studentOption(student_options);
+    //   break;
 
     case "my courses":
-      const my_courses = _student?.courses_enroll.map(
-        (course) => course.course_name
-      );
-      console.log(`You have enrolled in these courses, ${my_courses}`);
-      status = await studentOption(student_options);
+      const my_courses = _student?.courses_enroll
+        .map((course) => course.course_name)
+        .concat(["~main_menu", "~exit"]);
+
+      if (my_courses?.length && my_courses?.length > 2) {
+        console.log(`You have enrolled in these courses, ${my_courses}`);
+        const course_name: string = await studentOption(my_courses);
+        const updated_course = courses.find(
+          (course) => course.course_name === course_name
+        );
+        _course = updated_course ? { ...updated_course } : undefined;
+
+        status = `${course_name}-enrolled`;
+      } else {
+        console.log(
+          `You are not currently enrolled in any course, select enroll option to participate.`
+        );
+        status = await studentOption(student_options);
+      }
       break;
 
     case "show status":
@@ -186,21 +203,78 @@ while (status !== "~exit") {
 
     // enrolling in the course.
     case "metaverse":
-      _course = courses.find((course) => course.course_name === "metaverse");
-      _student?.enroll(_course);
-      status = await studentOption(student_options);
+      {
+        const updated_course = courses.find(
+          (course) => course.course_name === "metaverse"
+        );
+        _course = updated_course ? { ...updated_course } : undefined;
+        _student?.enroll(_course);
+        status = await studentOption(student_options);
+      }
       break;
-
     case "blockchain":
-      _course = courses.find((course) => course.course_name === "blockchain");
-      _student?.enroll(_course);
-      status = await studentOption(student_options);
+      {
+        const updated_course = courses.find(
+          (course) => course.course_name === "blockchain"
+        );
+        _course = updated_course ? { ...updated_course } : undefined;
+        _student?.enroll(_course);
+        status = await studentOption(student_options);
+      }
       break;
 
     case "cnc":
-      _course = courses.find((course) => course.course_name === "cnc");
-      _student?.enroll(_course);
-      status = await studentOption(student_options);
+      {
+        const updated_course = courses.find(
+          (course) => course.course_name === "cnc"
+        );
+        _course = updated_course ? { ...updated_course } : undefined;
+        _student?.enroll(_course);
+        status = await studentOption(student_options);
+      }
+      break;
+
+    // course actions.
+    case "metaverse-enrolled":
+      status = await studentOption(course_options);
+      break;
+
+    case "blockchain-enrolled":
+      status = await studentOption(course_options);
+      break;
+
+    case "cnc-enrolled":
+      status = await studentOption(course_options);
+      break;
+
+    case "course status":
+      console.log({ _course });
+      status = await studentOption(course_options);
+      break;
+
+    case "pay fee":
+      if (_course?.is_fee_paid) {
+        console.log("Bohut Paisa hen kiya, Already Paid...");
+      } else if (_course && !_course?.is_fee_paid) {
+        _course.is_fee_paid = true;
+        console.log(`Paid Fee Successfully of ${_course?.course_name} course.`);
+        if (_student?.courses_enroll.length) {
+          const updated_courses_enroll: CoursesInterface[] =
+            _student?.courses_enroll.map((course) =>
+              course.course_id === _course?.course_id ? _course : course
+            );
+          const updated_student: StudentInterface = {
+            ..._student,
+            courses_enroll: updated_courses_enroll,
+          };
+          const student_index = students.findIndex((student) =>
+            student.id === updated_student.id ? updated_student : student
+          );
+          students.splice(student_index, 1, updated_student);
+        }
+      }
+
+      status = await studentOption(course_options);
       break;
 
     case "~exit":
